712598b517bc92e7235cfe7b5d46227f8ee6dfc6
Chinedum <chinedumike85@gmail.com>
2023-01-03 18:46:31 +0100

    Basic react set-up complete

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,18 @@
 import React from 'react'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import Netflix from "./pages/Netflix";
 
 const App = () => {
   return (
-    <div>Hello</div>
+    <BrowserRouter>
+      <Routes>
+        <Route exact path="/login" element={<Login />} />
+        <Route exact path="/signup" element={<Signup />} />
+        <Route exact path="/" element={<Netflix />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
